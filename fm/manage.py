1cb05fdad5d04cd20364e61a76a746cc15b30791
Chris Reeves <chris@thisissoon.com>
2015-03-05 21:00:51 +0000

    Abolity to run sev server under ssl

--- fm/manage.py
+++ fm/manage.py
@@ -9,7 +9,7 @@ FM Management Command Scripts.
 """
 
 from flask.ext.migrate import Migrate, MigrateCommand
-from flask.ext.script import Manager, Server
+from flask.ext.script import Manager
 from fm import app
 from fm.ext import db
 
@@ -18,8 +18,24 @@ app = app.create()
 manager = Manager(app)
 migrate = Migrate(app, db)
 
-manager.add_command("runserver", Server(host='0.0.0.0'))
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def runserver(ssl=False, host='0.0.0.0', port=5000):
+    """ Customised run server function so we can run the development server
+    with custom arguments.
+    """
+
+    kwagrs = {
+        'host': host,
+        'port': port
+    }
+
+    if ssl:
+        kwagrs['ssl_context'] = 'adhoc'
+
+    app.run(**kwagrs)
 
 
 def run():
