4383604e6042e0c064389f418d3b6a2be306bffd
Chris Reeves <chris@thisissoon.com>
2015-03-14 14:53:32 +0000

    Test module organisation

---
+++ tests/views/test_queue.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+tests.views.player.test_queue
+=============================
+
+Unit tests for the ``fm.views.player.QueueView`` class.
+"""
+
+
+class TestGet(object):
+    """Player Queue: GET
+    """
+
+    pass
