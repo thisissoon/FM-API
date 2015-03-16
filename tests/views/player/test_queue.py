3aa38a76d4bbc31efcb278aa3975ced6bcc39a49
Chris Reeves <chris@thisissoon.com>
2015-03-16 08:43:58 +0000

    Fleshing out queue unit tests

--- tests/views/player/test_queue.py
+++ tests/views/player/test_queue.py
@@ -11,6 +11,7 @@ Unit tests for the ``fm.views.player.QueueView`` class.
 import mock
 
 from fm.ext import db
+from fm.serializers.spotify import TrackSerialzier
 from flask import url_for
 from tests.factories.spotify import TrackFactory
 
@@ -40,3 +41,32 @@ class TestGetQueue(object):
         response = self.client.get(url)
 
         assert len(response.json) == 6
+
+    def must_return_empty_list_when_no_queue(self):
+        self.redis.lrange.return_value = []
+        self.redis.llen.return_value = 0
+
+        url = url_for('player.queue')
+        response = self.client.get(url)
+
+        assert len(response.json) == 0
+
+    def should_return_200_ok(self):
+        tracks = [TrackFactory(), TrackFactory(), TrackFactory()]
+
+        db.session.add_all(tracks)
+        db.session.commit()
+
+        # Each track is in the queue twice
+        queue = [t.spotify_uri for t in tracks]
+
+        self.redis.lrange.return_value = queue
+        self.redis.llen.return_value = len(tracks)
+
+        url = url_for('player.queue')
+        response = self.client.get(url)
+
+        expected = TrackSerialzier().serialize(tracks, many=True)
+
+        assert response.status_code == 200
+        assert expected == response.json
