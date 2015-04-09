bde7b02de1ffa7ff4aebda220db0266d5c3363f4
Chris Reeves <chris@thisissoon.com>
2015-04-09 10:29:08 +0100

    Renaming raw to data

--- fm/tasks/queue.py
+++ fm/tasks/queue.py
@@ -14,12 +14,12 @@ from fm.logic.player import Queue
 
 
 @celery.task
-def add(raw, user):
+def add(data, user):
     """ Celery task for adding a single track to the queue.
 
     Arguments
     ---------
-    raw : dict
+    data : dict
         Raw Decoded Spotify API Track JSON data
     user : str
         The name of the user whom added the track to the queue
@@ -27,26 +27,26 @@ def add(raw, user):
 
     # Create or Update Album
 
-    album = Album.query.filter(Album.spotify_uri == raw['album']['uri']).first()
+    album = Album.query.filter(Album.spotify_uri == data['album']['uri']).first()
     if album is None:
         album = Album()
 
-    album.name = raw['album']['name']
-    album.images = raw['album']['images']
-    album.spotify_uri = raw['album']['uri']
+    album.name = data['album']['name']
+    album.images = data['album']['images']
+    album.spotify_uri = data['album']['uri']
 
     db.session.add(album)
     db.session.commit()
 
     # Crate or Update Track
 
-    track = Track.query.filter(Track.spotify_uri == raw['uri']).first()
+    track = Track.query.filter(Track.spotify_uri == data['uri']).first()
     if track is None:
         track = Track()
 
-    track.name = raw['name']
-    track.spotify_uri = raw['uri']
-    track.duration = raw['duration_ms']
+    track.name = data['name']
+    track.spotify_uri = data['uri']
+    track.duration = data['duration_ms']
     track.album_id = album.id
 
     db.session.add(track)
@@ -54,7 +54,7 @@ def add(raw, user):
 
     # Create or Update Artists - Appending Album to the Artists Albums
 
-    for item in raw['artists']:
+    for item in data['artists']:
         artist = Artist.query.filter(Artist.spotify_uri == item['uri']).first()
         if artist is None:
             artist = Artist()
