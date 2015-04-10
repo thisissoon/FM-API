24cd7af4cb13fdb8a4059383c47a9fc3b70534e6
Chris Reeves <chris@thisissoon.com>
2015-04-10 10:58:17 +0100

    Missing doc strings

--- fm/serializers/types/spotify.py
+++ fm/serializers/types/spotify.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+fm.serializers.types.spotify
+============================
+
+Custom Kim Field Types relating to Spotifu Validation
+"""
+
 import httplib
 import kim.types as t
 import requests
@@ -7,13 +17,38 @@ from simplejson import JSONDecodeError
 
 
 class SpotifyURI(t.String):
+    """ Custom field which validates a Spotify URI with the spotify API. On
+    marshal the returl value will be the decoded JSON data from the Spotify
+    API.
+    """
 
     track = None
 
     def marshal_value(self, value):
+        """ Return track data, if present, else returns None.
+
+        Arguments
+        ---------
+        value : str
+            The Spotiy URI
+
+        Returns
+        -------
+        dict or None
+            Track data from validation or None if not valid
+        """
+
         return self.track
 
     def validate(self, value):
+        """ Validates the value with Spotify.
+
+        Arguments
+        ---------
+        value : str
+            The Spotiy URI
+        """
+
         id = value.split(':')[-1]
         endpoint = 'https://api.spotify.com/v1/tracks/{0}'.format(id)
 
