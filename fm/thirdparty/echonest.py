30a9bb7cbd19035ced665e33125dd63cfd4a4ab0
James Warren <mail@jamesjwarren.com>
2015-07-28 18:48:15 +0100

    retrieve track analysis from echo nest

--- fm/thirdparty/echonest.py
+++ fm/thirdparty/echonest.py
@@ -77,3 +77,51 @@ def get_artist_genres(uri):
         )
 
     return [genre['name'] for genre in genres]
+
+
+def get_track_analysis(uri):
+    """ Retrieves a Spotify Tracks audio profile from Echo Nest.
+
+    Arguments
+    --------
+    uri: str
+        The track spotify URI
+
+    Returns
+    -------
+    dict
+        Track audio summary
+    """
+
+    url = furl(BASE_URL) \
+        .add(path=['track', 'profile']) \
+        .add({
+            'api_key': config.ECHONEST_API_KEY,
+            'format': 'json',
+            'id': uri,
+            'bucket': 'audio_summary'
+        }).url
+
+    try:
+        response = requests.get(url)
+    except requests.ConnectionError:
+        raise EchoNestError('Error connecting to the Echonest API')
+
+    if not response.status_code == httplib.OK:
+        raise EchoNestError(
+            'Echonest API response code was {0} not 200'.format(response.status_code)
+        )
+
+    try:
+        data = response.json()
+    except JSONDecodeError:
+        raise EchoNestError('Unable to decode response data to dict')
+
+    try:
+        analysis = data['response']['track']['audio_summary']
+    except KeyError as e:
+        raise EchoNestError(
+            'Response payload does not contain {0} element'.format(e.message)
+        )
+
+    return analysis
