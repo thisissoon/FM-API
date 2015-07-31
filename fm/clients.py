f4a910c7ef6005c36f5580d68a5a2d5472a36c8b
Chris Reeves <chris@thisissoon.com>
2015-07-31 14:34:15 +0100

    Simple known_client decorator / valid_request stub

---
+++ fm/clients.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+fm.clients
+==========
+
+Allow known external clients that are not users access to the API, these could
+be the Player or Volume control systems for example which are physical boxes
+that run outside of the network perimeter.
+
+Clients should send a Client-ID header which will correlate to a Private Key
+stored both on the Client and the Server. The Client will encode their request
+with this key, the server will check the request validity by also encoding
+the raw request with the same key and ensuring they match, this is known as
+HMAC.
+"""
+
+# Standard Libs
+from functools import wraps
+
+# First Party Libs
+from fm.http import Unauthorized
+
+
+def known_client(function):
+    """ Decorator which requires that the view is accessible by only
+    known external clients.
+    """
+
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        if not valid_request():
+            return Unauthorized()
+        return function(*args, **kwargs)
+
+    return wrapper
+
+
+def valid_request():
+    pass
